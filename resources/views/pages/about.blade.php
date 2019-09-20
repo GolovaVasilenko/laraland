b539feaa4171f2c7c5df329d9a92fe0be38578a3
GolovaVasilenko <vasilenko75@gmail.com>
2019-09-20 11:36:18 +0300

    fixed bugs for about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -9,6 +9,7 @@
             <h1>{{$page->title}}</h1>
         </div>
         @foreach($body as $b)
+            @break(empty($images))
         <div class="row">
             <div class="gallery-wrapper @if($count%2 == 0) invert @endif popup-gallery">
                 <div class="first-item gallery-thumb-box">
@@ -24,11 +25,18 @@
                     <a href="assets/images/{{ $img }}"><img src="assets/images/{{ $img }}" alt="" /></a>
                 </div>
                 <div class="text-item gallery-thumb-box">
-                     <p>{{ $b }}.</p>
+                     <p>{{ array_shift($body) }}.</p>
                 </div>
             </div>
         </div>
         @endforeach
+            @if(!empty($body))
+                <div class="other-text text-item">
+                @foreach($body as $b)
+                    <p>{{ $b }}</p>
+                @endforeach
+                </div>
+            @endif
         @else
             {{ "Db Empty!" }}
         @endif
